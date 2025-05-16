356cc466bbdd34e8606b9fb7ce85e885c5cdc830
Sofus Addington <sofus@addington.dk>
2025-05-16 14:26:49 +0200

    Add ghostty

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,12 @@
 
 set -ouex pipefail
 
-# dnf5 -y copr enable pgdev/ghostty
+dnf5 -y copr enable pgdev/ghostty
 dnf5 -y copr enable yalter/niri-git
+dnf5 -y copr enable vedantmgoyal/rio
 
 dnf5 install -y niri rust-glib-sys-devel rust-gstreamer-devel
+dnf5 install -y rio
 
 systemctl enable podman.socket
 mkdir /nix
