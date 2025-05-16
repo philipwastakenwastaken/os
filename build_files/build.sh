d7fd0be2a0fe894d77f81e68d9ccce2fed82b074
Sofus Addington <sofus@addington.dk>
2025-05-16 12:42:10 +0200

    Add initial changes

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,10 @@
 
 set -ouex pipefail
 
-### Install packages
+# dnf5 -y copr enable pgdev/ghostty
+dnf5 -y copr enable yalter/niri-git
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+dnf5 install -y niri rust-glib-sys-devel rust-gstreamer-devel
 
 systemctl enable podman.socket
+mkdir /nix
