5564d6f656b9a85ef793c2859445ad44045ddd55
Sofus Addington <sofus@addington.dk>
2025-05-18 20:17:23 +0200

    more fixing paths

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,32 +4,19 @@ set -ouex pipefail
 
 mkdir /nix
 
-# Node
-#mkdir /usr/node
-mkdir -p $(realpath /root)
-# export NPM_CONFIG_PREFIX=/usr/node/
-# export NPM_CONFIG_PREFIX=/usr
-# mkdir $NPM_CONFIG_PREFIX
-# export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
-dnf5 install -y npm
-#npm config set prefix $NPM_CONFIG_PREFIX
-npm install -g --prefix /usr @angular/cli @angular/language-service typescript @angular/language-server
-
 # Rust
 dnf5 install -y cargo
 
-# export CARGO_HOME=/tmp/cargo
-# export PATH="$CARGO_HOME/bin:$PATH"
-
-# mkdir -p "$CARGO_HOME"
+export CARGO_HOME=/tmp/cargo
+mkdir -p "$CARGO_HOME"
 
 # Cargo binstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 # Helix
 dnf5 install -y clang
-# export HELIX_DEFAULT_RUNTIME=/usr/lib/helix/runtime
-# mkdir -p "$HELIX_DEFAULT_RUNTIME"
+export HELIX_DEFAULT_RUNTIME=/usr/lib/helix/runtime
+mkdir -p "$HELIX_DEFAULT_RUNTIME"
 git clone -b pull-diagnostics https://github.com/SofusA/helix-pull-diagnostics.git
 cd helix-pull-diagnostics
 cargo build --profile opt --locked
@@ -60,21 +47,25 @@ cargo install --root /usr --git https://github.com/sofusa/qobuz-player
 # Dotnet
 dnf5 install -y dotnet-sdk-9.0 aspnetcore-runtime-9.0 azure-cli
 
-# export DOTNET_CLI_HOME=/usr/dotnet
-# export PATH="$DOTNET_CLI_HOME/bin:$PATH"
-# mkdir -p "$DOTNET_CLI_HOME"
-dotnet tool install --tool-path /usr csharpier
+dotnet tool install --tool-path /usr/bin csharpier
 # TODO: azure core functions bicep-langserver powershell Azure Artifacts Credential Provider
 # wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
 
 # Shell
 dnf5 install -y zoxide atuin fd-find ripgrep
-cargo binstall --root /usr sd rpg-cli eza zellij
+cargo binstall --git https://github.com/facundoolano/rpg-cli
+cargo binstall --root /usr sd eza zellij
 cargo binstall --strategies crate-meta-data --root /usr yazi-cli
 
 # Git
 dnf5 install -y gh meld
 cargo binstall --root /usr lazyjj 
 cargo binstall --root /usr --strategies crate-meta-data jj-cli
+
+# Node
+mkdir -p $(realpath /root)
+dnf5 install -y npm
+#npm config set prefix $NPM_CONFIG_PREFIX
+npm install -g --prefix /usr @angular/cli @angular/language-service typescript @angular/language-server
 
 systemctl enable podman.socket
