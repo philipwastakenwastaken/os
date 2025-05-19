769983e6fc08638417962baa89a08202498e30ca
SofusA <sofus@addington.dk>
2025-05-19 13:11:41 +0200

    Pwsh

--- build_files/build.sh
+++ build_files/build.sh
@@ -57,8 +57,7 @@ dotnet tool install --tool-path /usr/bin csharpier
 # vscode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-#dnf5 check-update
-dnf5 install -y code
+dnf5 install -y code powershell
 
 # Shell
 dnf5 install -y zoxide atuin fd-find ripgrep
