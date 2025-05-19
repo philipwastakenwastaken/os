6eb02f6d77405de5a8da0d7a42150aa38bf241ec
SofusA <sofus@addington.dk>
2025-05-19 11:34:07 +0200

    Code

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,8 +3,14 @@
 set -ouex pipefail
 
 mkdir /nix
-
 mkdir -p $(realpath /root)
+
+# vscode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+#dnf5 check-update
+dnf5 install code
+
 
 # Rust
 dnf5 install -y cargo
