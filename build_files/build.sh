68cd9ea9d60c945a5a55995aaaa2050800ee401f
SofusA <sofus@addington.dk>
2025-05-19 16:48:54 +0200

    Pwsh 3

--- build_files/build.sh
+++ build_files/build.sh
@@ -57,7 +57,7 @@ dotnet tool install --tool-path /usr/bin csharpier
 # vscode pwsh
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+curl https://packages.microsoft.com/config/rhel/9/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 dnf5 install -y code powershell
 
 # Shell
