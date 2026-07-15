943ee86570080931b21f39921daf1165ab3884cd
Philip Tofte Nielsen <pni@cvation.com>
2026-07-15 17:52:04 +0200

    fix ghostty install

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,7 +7,9 @@ dnf5 -y install \
     niri \
     waybar
 
-dnf5 -y install --enable-repo=terra ghostty
+curl -fsSL "https://copr.fedorainfracloud.org/coprs/scottames/ghostty/repo/fedora-$(rpm -E %fedora)/scottames-ghostty-fedora-$(rpm -E %fedora).repo" \
+    -o /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:scottames:ghostty.repo
+dnf5 -y install ghostty
 
 mkdir /nix
 
